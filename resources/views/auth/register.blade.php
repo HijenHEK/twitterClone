f6712c05b65168760264650767ab63b7f1d7a4c7
HijenHek <k.hijen@gmail.com>
2020-11-10 10:17:55 +0100

    reg style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-    @if ($errors->any())
-        <div>
-            <div>{{ __('Whoops! Something went wrong.') }}</div>
+   <div class="container">
 
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+        <form class="c mt5" method="POST" action="{{ route('register') }}">
+            @csrf
 
-        <div>
-            <label>{{ __('Name') }}</label>
-            <input type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
-        </div>
+            <div class="group f jb ac">
+                <label>{{ __('Name') }}</label>
+                <input type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
+            </div>
 
-        <div>
-            <label>{{ __('Email') }}</label>
-            <input type="email" name="email" value="{{ old('email') }}" required />
-        </div>
+            <div class="group f jb ac">
+                <label>{{ __('Email') }}</label>
+                <input type="email" name="email" value="{{ old('email') }}" required />
+            </div>
 
-        <div>
-            <label>{{ __('Password') }}</label>
-            <input type="password" name="password" required autocomplete="new-password" />
-        </div>
+            <div class="group f jb ac">
+                <label>{{ __('Password') }}</label>
+                <input type="password" name="password" required autocomplete="new-password" />
+            </div>
 
-        <div>
-            <label>{{ __('Confirm Password') }}</label>
-            <input type="password" name="password_confirmation" required autocomplete="new-password" />
-        </div>
+            <div class="group f jb ac">
+                <label>{{ __('Confirm Password') }}</label>
+                <input type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
 
-        <a href="{{ route('login') }}">
-            {{ __('Already registered?') }}
-        </a>
+            <div class="group f jc mt2">
+                    <a class="link " href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+            </div>
 
-        <div>
-            <button type="submit">
-                {{ __('Register') }}
-            </button>
-        </div>
-    </form>
+            <div class="group f jc">
+                <button class="btn btn-primary " type="submit">
+                    {{ __('Register') }}
+                </button>
+            </div>
+        </form>
+
+        @if ($errors->any())
+            <div class="errors c card">
+                <div class="card-head ">{{ __('Whoops! Something went wrong.') }}</div>
+
+                <ul class="card-body  ">
+                    @foreach ($errors->all() as $error)
+                        <li class="errors-item">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+   </div>
 @endsection
