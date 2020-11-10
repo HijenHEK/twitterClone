81ac374527173e146a0b9ec0bf1daf44ab8de703
HijenHek <k.hijen@gmail.com>
2020-11-10 07:53:58 +0100

    fresh laravel + fortify no fe

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Fortify;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Fortify::viewPrefix('auth.');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('home', 'home')
+	->name('home')
+	->middleware(['auth', 'verified']);
