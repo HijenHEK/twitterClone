c118b63074f138e4d9812be0240901f90be5e20c
HijenHek <k.hijen@gmail.com>
2020-11-13 16:41:00 +0100

    unlike

--- app/Traits/Likable.php
+++ app/Traits/Likable.php
@@ -10,10 +10,21 @@ trait Likable
 
 
 
-    public function like($user , $liked = true){
+    public function like(User $user , $liked = true){
+
+        $user = $user ? $user : Auth::user();
+
+        if($this->liked(Auth::user() , $liked)) {
+
+            return $this->likes()->where('user_id',$user->id)
+                                 ->where('tweet_id',$this->id)
+                                 ->where('liked',$liked)
+                                 ->delete() ;
+        }
+
         return $this->likes()->updateOrCreate(
             [
-                "user_id" => $user ? $user->id : Auth::user()->id ,
+                "user_id" => $user->id ,
                 "tweet_id" => $this->id
             ],[
                 "liked" => $liked
